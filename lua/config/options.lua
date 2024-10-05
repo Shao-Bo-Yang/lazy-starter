0e2dea419fdd0f83daef5bfe2b8cd2b8027f9126
y <yangshaobo1996@gmail.com>
2024-10-06 04:47:23 +0800

    update

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,10 +1,13 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
--- vim.opt.shiftwidth = 4
--- vim.opt.tabstop = 8
+vim.opt.shiftwidth = 4
+vim.opt.tabstop = 4
 -- vim.opt.softtabstop = 0
--- vim.opt.expandtab = true
+vim.opt.expandtab = true
 vim.opt.relativenumber = false
 vim.g.lazyvim_python_lsp = "basedpyright"
+vim.opt.linebreak = true
+vim.opt.wrap = true
+vim.opt.spell = true
 -- vim.g.lazyvim_python_lsp = "pyright"
